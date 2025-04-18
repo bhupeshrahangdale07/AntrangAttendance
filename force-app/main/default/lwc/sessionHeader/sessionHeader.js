2f7eeb8d5e32afee557099451cd17d790bcd3b64
Bhupesh Rahangdale <bhupeshrahangdale07@gmail.com>
2025-04-18 12:56:13 +0530

    Session Header and Login Page

---
+++ force-app/main/default/lwc/sessionHeader/sessionHeader.js
@@ -0,0 +1,8 @@
+/*Description :
+    Session Header component for Session Scheduling and Attendance Form*/
+import { LightningElement } from 'lwc';
+import Antarang_logo from "@salesforce/resourceUrl/antarangNewLogo";
+export default class SessionHeader extends LightningElement {
+    
+    antarangLogo = Antarang_logo;
+}
---
+++ force-app/main/default/lwc/sessionLogin/sessionLogin.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import Antarang_logo from "@salesforce/resourceUrl/Antarang_logo_Asset_1";
+export default class SessionLogin extends LightningElement {
+
+    antarangLogo = Antarang_logo;
+}
