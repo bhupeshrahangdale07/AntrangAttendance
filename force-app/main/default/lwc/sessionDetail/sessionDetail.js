633c9ece239238b1a6556959a632f71f133d7d94
Bhupesh Rahangdale <bhupeshrahangdale07@gmail.com>
2025-04-25 11:02:16 +0530

    SessionDetail Accordian

--- force-app/main/default/lwc/sessionDetail/sessionDetail.js
+++ force-app/main/default/lwc/sessionDetail/sessionDetail.js
@@ -7,12 +7,19 @@ import getdynamicpicklistval from '@salesforce/apex/SessionDetailController.getd
 import getBatch from '@salesforce/apex/SessionDetailController.getBatch';
 import sessionSignOut from '@salesforce/apex/SessionDetailController.sessionSignOut';
 import getSchoolList from '@salesforce/apex/SessionDetailController.getSchoolList';
+import lstsessionRecords from '@salesforce/apex/SessionDetailController.lstsessionRecords';
 import {NavigationMixin} from "lightning/navigation";
 let options = [
     { "label": "No School Found", "value": "" }
 ];
 
 export default class SessionDetail extends NavigationMixin(LightningElement) {
+    @track activeChildSession = [];
+    @track hasParentSessions = false;
+    @track hasParentSessions = false;
+    @track hasStudentSessions = false;
+    @track hasFlexibleSessions = false;
+    @track hasCounselingSessions = false;
     @track options = options;
     flagIndicatingDataHasBeenLoadedInVariables = false;
     grade;
@@ -33,7 +40,55 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
     activeSections = ['flexibleSession', 'studentSession','parentSession','individualCounsellingSession'];
     showParentSessionStatus=false;
     showCounsellingSessionStatus=false;
+    CounsellingSessionStatusValue='';
+    CounsellingSessionStatusClass='';
+    ParentSessionStatusValue= '';
+    ParentSessionStatusClass='';
+    @track parentSessions = [];
+    @track studentSessions = [];
+    @track counselingSessions = [];
+    @track flexibleSessions = [];
 
+    handleSectionToggleChild(event){
+        console.log('handleSectionToggleChild')
+        const openSections = event.detail.openSections;
+        console.log('openSections = ',openSections[0])
+        //this.activeChildSession = openSections;
+
+        // Update isOpen property for each session dynamically
+        /*this.flexibleSessions = this.flexibleSessions.map(session => {
+            return {
+                ...session,
+                isOpen: Array.isArray(openSections)
+                    ? openSections.includes(session.id)
+                    : openSections === session.id
+            };
+        });
+        this.studentSessions = this.studentSessions.map(session => {
+            return {
+                ...session,
+                isOpen: Array.isArray(openSections)
+                    ? openSections.includes(session.id)
+                    : openSections === session.id
+            };
+        });
+        this.parentSessions = this.parentSessions.map(session => {
+            return {
+                ...session,
+                isOpen: Array.isArray(openSections)
+                    ? openSections.includes(session.id)
+                    : openSections === session.id
+            };
+        });
+        this.counselingSessions = this.counselingSessions.map(session => {
+            return {
+                ...session,
+                isOpen: Array.isArray(openSections)
+                    ? openSections.includes(session.id)
+                    : openSections === session.id
+            };
+        });*/
+    }
     handleSectionToggle(event) {
         const openSections = event.detail.openSections;
         this.showParentSessionStatus = !openSections.includes('parentSession');
@@ -98,6 +153,7 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
         console.log(' this.schoolId = ', this.schoolId);
         this.getSchoolListFunc();
         this.batch = params.get('bId');
+        this.getSessionListFunc();
         this.grade = params.get('grade');
         this.getBatchFunc();
         this.hideModalBox();
@@ -153,6 +209,7 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
         this.showLoading = true;
         if (window.innerWidth < 768) {
             this.activeSections = [];
+            this.activeChildSession = [];
             this.showParentSessionStatus=true;
             this.showCounsellingSessionStatus=true;
         } else {
@@ -174,7 +231,7 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
             
         }else{
             console.log('b');
-            this.showToastMessage('Not allowing to work on multiple tab','error');
+            this.showToastMessage('This form is already open in another tab!','error');
             this.backToLogin();
         }
         
@@ -192,13 +249,15 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
 
     };
     onchangeGrade(event){
+        this.batch = '';
         this.grade = event.detail.value;
         this.getBatchFunc();
     }
     getBatchFunc(){
         getBatch({
             grade : this.grade,
-            schoolId : this.schoolId
+            schoolId : this.schoolId,
+            trainerId : this.facEmailId
         })
         .then(result => {
             console.log('getBatch = ',JSON.stringify(result));
@@ -213,7 +272,108 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
     }
     onchangeBatch(event){
         this.batch = event.detail.value;
+        this.parentSessions = [];
+        this.studentSessions = [];
+        this.flexibleSessions = [];
+        this.counselingSessions = [];
+        this.getSessionListFunc();
     }
+    getSessionListFunc(){
+        lstsessionRecords({
+            batchId : this.batch,
+            loggedInTrainer : this.facEmailId
+        })
+        .then(result => {
+            debugger
+            const response = [...result]
+            console.log(JSON.stringify(response));
+            if(response){
+                response.forEach(session => {
+                    session.startTimeFormatted = this.formatTimeToInput(session.startTime); 
+                    session.disableStartTimeNDate = session.isAttendenceFound || session.isNotLoggedInTrainerSession;
+                    session.isOpen = true;
+                    if (session.isParentSession) {
+                        this.parentSessions.push(session);
+                    }
+                    if (session.isStudentSession) {
+                        this.studentSessions.push(session);
+                    }
+                    if (session.isCounselingSession) {
+                        this.counselingSessions.push(session);
+                    }
+                    if (session.isFlexibleSession) {
+                        this.flexibleSessions.push(session);
+                    }
+                });
+                this.hasParentSessions = this.parentSessions && this.parentSessions.length > 0;
+                this.hasStudentSessions = this.studentSessions && this.studentSessions.length > 0;
+                this.hasFlexibleSessions = this.flexibleSessions && this.flexibleSessions.length > 0;
+                this.hasCounselingSessions = this.counselingSessions && this.counselingSessions.length > 0;
+                if (window.innerWidth < 768) {
+                    this.activeChildSession = [];
+                }else{
+                    this.activeChildSession = [
+                        ...this.parentSessions.map(session => session.id),
+                        ...this.studentSessions.map(session => session.id),
+                        ...this.counselingSessions.map(session => session.id),
+                        ...this.flexibleSessions.map(session => session.id)
+                    ];
+                }
+                /*console.log('this.activeChildSession = ',this.activeChildSession);
+                this.flexibleSessions = this.flexibleSessions.map(session => {
+                    return {
+                        ...session,
+                        isOpen: Array.isArray(this.activeChildSession)
+                            ? this.activeChildSession.includes(session.id)
+                            : this.activeChildSession === session.id
+                    };
+                });
+                this.parentSessions = this.parentSessions.map(session => {
+                    return {
+                        ...session,
+                        isOpen: Array.isArray(this.activeChildSession)
+                            ? this.activeChildSession.includes(session.id)
+                            : this.activeChildSession === session.id
+                    };
+                });
+                this.studentSessions = this.studentSessions.map(session => {
+                    return {
+                        ...session,
+                        isOpen: Array.isArray(this.activeChildSession)
+                            ? this.activeChildSession.includes(session.id)
+                            : this.activeChildSession === session.id
+                    };
+                });
+                this.counselingSessions = this.counselingSessions.map(session => {
+                    return {
+                        ...session,
+                        isOpen: Array.isArray(this.activeChildSession)
+                            ? this.activeChildSession.includes(session.id)
+                            : this.activeChildSession === session.id
+                    };
+                });*/
+                
+                
+            }
+            this.CounsellingSessionStatusValue= this.counselingSessions[0].status;
+            this.CounsellingSessionStatusClass=this.counselingSessions[0].statusClassName;
+
+            this.ParentSessionStatusValue= this.parentSessions[0].status;
+            this.ParentSessionStatusClass=this.parentSessions[0].statusClassName;
+
+            console.log('Parent Sessions:', this.parentSessions);
+            console.log('Student Sessions:', this.studentSessions);
+            console.log('Counseling Sessions:', this.counselingSessions);
+            console.log('Flexible Sessions:', this.flexibleSessions);
+            console.log('activeChildSession:', this.activeChildSession);
+            this.activeChildSession1 = [...this.activeChildSession];
+            
+        }).catch(error => {
+            console.log(error);
+             this.showToastMessage(error.body.message,'error');
+        });
+    }
+    @track activeChildSession1 = [];
     getdynamicpicklistvalFunc(){
         getdynamicpicklistval({
             objectName : 'Batch__c',
@@ -286,6 +446,27 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
             }.saveButton button.slds-button.slds-button_brand {
                 width: 7%;
                 height: 38px;
+            }.facNameInAccordion input{
+                background-color: #F09F4D !important;
+            }
+            .sessionAccordion input{
+                color: #49445C !important;
+                border: 1px solid #49445C !important;
+            }
+            .sessionAccordion label.slds-form-element__label.slds-no-flex, .sessionAccordion label.slds-form-element__label {
+                color: #49445C !important;
+                margin-bottom: 20px;
+            }.CompleteCls .slds-input, .CompleteCls button.slds-button.slds-button_brand{
+                background-color:#92DB72 !important;
+            }.ReadyforAttendanceCls .slds-input, .ReadyforAttendanceCls button.slds-button.slds-button_brand{
+                background-color:#9ABBFF !important;
+            }.ScheduledCls .slds-input, .ScheduledCls button.slds-button.slds-button_brand{
+                background-color:#EEDA5E !important;
+            }.UnscheduledCls .slds-input, .IncompleteCls .slds-input, .IncompleteCls button.slds-button.slds-button_brand, .UnscheduledCls button.slds-button.slds-button_brand{
+                background-color:#E88775 !important;
+            }button.slds-button.slds-button_brand {
+                width: 12%;
+                color: #000;
             }
             @media (max-width: 767px) {
                 lightning-button.completeButton button.slds-button.slds-button_brand, lightning-button.incompleteButton button.slds-button.slds-button_brand {
@@ -299,12 +480,44 @@ export default class SessionDetail extends NavigationMixin(LightningElement) {
             this.template.querySelector('div').appendChild(style);
         }
     }
+    isSessionOpen(sessionId) {
+        if (Array.isArray(this.activeChildSession)) {
+            return this.activeChildSession.includes(sessionId);
+        } else {
+            return this.activeChildSession === sessionId;
+        }
+    }
+    formatTimeToInput(s) {
+        if (!s) return '';
+        let time = new Date(s);
+        let hours = time.getUTCHours();
+        let minutes = time.getUTCMinutes();
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+
+        // Adjust hours for 12-hour format
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+
+        // Format minutes
+        minutes = minutes < 10 ? '0' + minutes : minutes; // pad single digits
+
+        return `${hours}:${minutes} ${ampm}`;
+    }
     
     handleSelectOption (event) {
       console.log('event.detail ='+event.detail);
       this.schoolId = event.detail;
       this.grade='';
       this.batch='';
+    }
+
+    value = '';
+
+    get options() {
+        return [
+            { label: 'Yes', value: 'Yes' },
+            { label: 'No', value: 'No' },
+        ];
     }
     /*@wire(getSchoolList)
     wiredAccounts({ error, data }) {
